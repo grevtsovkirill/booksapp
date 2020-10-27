027e448be73e8ada4d291f3aea18c0c63f6c226d
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-10-27 19:01:26 +0100

    add modal editForm

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -69,6 +69,42 @@
         </b-button-group>
       </b-form>
     </b-modal>
+    <b-modal ref="editBookModal"
+            id="bool-update-modal"
+            title="Update"
+            hide-footer>
+       <b-form @submit="onSubmitUpdate" @reset="onResetUdate" class="w-100">
+       <b-form-group id="form-title-edit-group"
+                  label="Title:"
+                  label-form="form-title-edit-input">
+            <b-form-input id="form-title-edit-input"
+                          type="text"
+                          v-model="editForm.title"
+                          required
+                          placeholder="Enter title">
+            </b-form-input>
+       </b-form-group>
+       <b-form-group id="form-author-edit-group"
+                  label="Author:"
+                  label-for="form-author-edit-input">
+            <b-form-input id="form-author-edit-input"
+                      type="text"
+                      v-model="editForm.author"
+                      required
+                      placeholder="Enter author">
+            </b-form-input>
+       </b-form-group>
+       <b-form-group id="form-read-edit-group">
+       <b-form-checkbox-group v-model="editForm.read" id="form-checks">
+         <b-form-checkbox value="true">Read?</b-form-checkbox>
+       </b-form-checkbox-group>
+       </b-form-group>
+      <b-button-group>
+        <b-button type="submit" variant="primary">Update</b-button>
+        <b-button type="reset" variant="danger">Cancel</b-button>
+      </b-button-group>
+      </b-form>
+    </b-modal>
       </b-col>
     </b-row>
   </b-container>
@@ -89,6 +125,12 @@ export default {
       },
       message: '',
       showMessage: false,
+      editForm: {
+        id: '',
+        title: '',
+        author: '',
+        read: [],
+      },
     };
   },
   components: {
