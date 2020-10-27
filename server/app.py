0f2ae9817430cc0c187e8278bd205f7b04e9a148
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-10-27 18:51:44 +0100

    handle single book by id - PUT method

--- server/app.py
+++ server/app.py
@@ -56,5 +56,26 @@ def all_books():
         response_object['books'] = BOOKS
     return jsonify(response_object)
 
+@app.route('/books/<book_id>', methods=['PUT'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book updated'
+    return jsonify(response_object)
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+    return False
+
 if __name__ == '__main__':
     app.run()
