bcef43945758c03ac3a583d35b9862e5a2950893
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-10-27 16:29:29 +0100

    add books page

--- server/app.py
+++ server/app.py
@@ -13,11 +13,34 @@ app.config.from_object(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
 
+@app.route('/books',methods=['GET'])
+def all_books():
+    return jsonify(
+        {'status': 'success',
+        'books': BOOKS}
+    )
 
 if __name__ == '__main__':
     app.run()
