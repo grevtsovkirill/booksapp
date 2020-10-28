a6fccc2be93e9b2316f1e98270f30017cfc1502d
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-10-28 15:49:47 +0100

    remove localhost links

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -148,7 +148,7 @@ export default {
   },
   methods: {
     getBooks() {
-      const path = 'http://localhost:5000/books';
+      const path = '/books';
       axios.get(path)
         .then((res) => {
           this.books = res.data.books;
@@ -159,7 +159,7 @@ export default {
         });
     },
     addBook(payload) {
-      const path = 'http://localhost:5000/books';
+      const path = '/books';
       axios.post(path, payload)
         .then(() => {
           this.getBooks();
@@ -215,7 +215,7 @@ export default {
       this.updateBook(payload, this.editForm.id);
     },
     updateBook(payload, bookID) {
-      const path = `http://localhost:5000/books/${bookID}`;
+      const path = `/books/${bookID}`;
       axios.put(path, payload)
         .then(() => {
           this.getBooks();
@@ -235,7 +235,7 @@ export default {
       this.getBooks();
     },
     removeBook(bookID) {
-      const path = `http://localhost:5000/books/${bookID}`;
+      const path = `/books/${bookID}`;
       axios.delete(path)
         .then(() => {
           this.getBooks();
